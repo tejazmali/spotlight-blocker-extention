85197acd9ca09ff39c80a26c6fefbae0f41fefb3
tejazmali <malitejas28@gmail.com>
2024-12-03 20:06:56 +0530

    tampermonkey

---
+++ tampermonkey.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         Block Swiper Wrapper
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Hide or block elements with the class .swiper-wrapper
+// @author       Your Name
+// @match        https://web.snapchat.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Function to block elements with .swiper-wrapper
+    function blockSwiperWrapper() {
+        const swiperElements = document.querySelectorAll('.swiper-wrapper');
+        swiperElements.forEach(element => {
+            element.style.display = 'none'; // Hide the element
+        });
+    }
+
+    // Observe changes in the DOM to block dynamically loaded content
+    const observer = new MutationObserver(blockSwiperWrapper);
+    observer.observe(document.body, { childList: true, subtree: true });
+
+    // Initial execution
+    blockSwiperWrapper();
+})();
